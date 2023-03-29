a71eba4aa7107fca55951fd6edc6c97db9e52ef6
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-03-29 14:19:28 -0700

    adding middleware

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello Dingleberry');
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    res.render('hello-gif', {gifUrl});
+    });
+
+app.get('/greetings/:name', (req, res) => {
+    const name = req.params.name;
+    res.render('greetings', {name});
     });
 
 // Start Server
