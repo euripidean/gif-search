0500885171021cdf288005ce862b3a4b211acfc7
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-03-29 15:00:21 -0700

    added working gif search

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// bring in the .env variables
+require('dotenv').config();
+TENOR_API_KEY = process.env.TENOR_API_KEY;
+
 const express = require('express');
 
 const app = express();
@@ -8,10 +12,23 @@ const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+const Tenor = require("tenorjs").client({
+    "Key": TENOR_API_KEY, // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", "notsafe"
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 // Routes
 app.get('/', (req, res) => {
-    res.render('home');
-    console.log(req.query)
+    term = ""
+    if (req.query.term) {
+        term = req.query.term;
+        }
+    Tenor.Search.Query(term, "10")
+    .then(response => {
+        const gifs = response;
+        res.render('home', {gifs})
+      }).catch(console.error);
     });
 
 app.get('/greetings/:name', (req, res) => {
